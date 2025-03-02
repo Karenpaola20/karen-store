16c359d967df9fab6cb5cf2ea6e4f02a2dc1421f
Karenpaola20 <kbuelvas899@gmail.com>
2025-03-02 17:06:24 -0500

    genere componentes

---
+++ src/app/componentes/item-carrito/item-carrito.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-carrito',
+  templateUrl: './item-carrito.component.html',
+  styleUrls: ['./item-carrito.component.scss'],
+})
+export class ItemCarritoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
